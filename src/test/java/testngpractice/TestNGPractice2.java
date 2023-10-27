94155b11174df5a65a5bd683454867a3f593e53e
suryaprakashraju <149137481+suryaprakashraju@users.noreply.github.com>
2023-10-27 11:12:39 +0530

    Complete Code Push

---
+++ src/test/java/testngpractice/TestNGPractice2.java
@@ -0,0 +1,35 @@
+package testngpractice;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class TestNGPractice2 {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver.get("https://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("chandrababunaidu");
+	  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	  
+  }
+  
+  @Test
+  public void selenium () {
+	  driver.get("http://www.seleniumlearn.com");
+	//  driver.findElement(By.xpath(null))
+	  
+	  
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver=new ChromeDriver();
+	  driver.manage().window().maximize();
+	  
+  }
+
+}
